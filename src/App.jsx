e5e687870d73545f7c6479074ab244ae5a17c088
rajeshknagulsamy@gmail.com <162724660+Rajeshknulsamy@users.noreply.github.com>
2024-09-12 16:26:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div className='sm:text-left text-black md:text-green-400'>
+      hey remo this is a  makeover project i do something different in doing that project because of this is a my 1st time effort of in this projects
+    </div>
+    </>
+  )
+}
+
+export default App
